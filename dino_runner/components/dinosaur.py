118b9d0d725b3afd586ac322beb69c4824a2f5d1
Luiz Eduardo <edutj@uol.com.br>
2022-11-22 21:09:56 -0300

    AULA 02: obstacle, obstacle_ manager, cactus

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,15 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
 Y_POS_DUCK = 340
 JUMP_VEL = 8.5
-DUCK_VEL = 8.5
-class Dinosaur:
+
+
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -16,8 +19,7 @@ class Dinosaur:
         self.jump_vel = 8.5
         self.dino_run = True
         self.dino_jump = False 
-        self.dino_duck = False  
-             
+        self.dino_duck = False               
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -40,16 +42,17 @@ class Dinosaur:
     def duck(self): 
         self.image = DUCKING[0]if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = X_POSduck
+        self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1 
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
-        if self.dino_jump:
+        elif self.dino_jump:
             self.jump()
-        if self.dino_duck:
+        elif self.dino_duck:
             self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
@@ -59,8 +62,8 @@ class Dinosaur:
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
-            self.dino_jump = False ###
-        elif not (self.dino_jump or user_input[pygame.K_DOWN]):
+            self.dino_jump = False         
+        elif not self.dino_jump and not self.dino_duck:    
             self.dino_duck = False
             self.dino_run = True  
             self.dino_jump = False 
